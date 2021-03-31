6344d73a5eca66588e99d93f0649d516edf4b8b8
samroodAli <devsamrood@gmail.com>
2021-03-31 19:55:30 +0530

    changed user_name to name in microposts model

---
+++ db/migrate/20210331142517_rename_user_name_to_name_in_microposts.rb
@@ -0,0 +1,4 @@
+class RenameUserNameToNameInMicroposts < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_131301) do
+ActiveRecord::Schema.define(version: 2021_03_31_142517) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
